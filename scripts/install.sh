24f98f7e73b147931268ebea89fcf9dbd232afec
Vaughan Rouesnel <vrouesnel@gmail.com>
2013-01-09 21:55:26 +1100

    Add shims to Underscore and Backbone for use with Bower. Add Bower as a dependency.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 # Add a line for each submodule which needs to be installed.
 #(cd lib/foo && npm install)
 
-# Install vendor assets with Bower
-# TODO: See how this goes because it will be slow on Heroku because of the ephemeral file system for caching.
+# Install vendor assets with Bower.
 bower install
